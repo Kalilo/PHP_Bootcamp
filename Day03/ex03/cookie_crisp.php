8950c73cd28452006f2e0205bef34218d3737886
Kalilo <kalilohansmann@gmail.com>
2016-09-16 16:15:29 +0200

    added fixes

--- Day03/ex03/cookie_crisp.php
+++ Day03/ex03/cookie_crisp.php
@@ -1,20 +1,39 @@
 <?php
 	$action = NULL;
-	foreach ($_GET as $key => $value)
+	$name = NULL;
+	$val = NULL;
+	$k = $_GET;
+	$k[] = "1";
+	foreach ($k as $key => $value)
 	{
 		if (!strcmp($key, "action"))
 		{
 			if (!strcmp($value, "set") || !strcmp($value, "del") || !strcmp($value, "get"))
 				$action = $value;
 		}
-		else
+		else if (!strcmp($key, "name") || !strcmp($key, "value"))
 		{
-			if (!strcmp($action, "set"))
-				setcookie($key, $value, time() + 86400, "/");
-			else if (!strcmp($action, "del"))
-				setcookie($key, $value, time() - 1000, "/");
-			else if (!strcmp($action, "get"))
-				echo "{$_COOKIE[$key]}\n";
+			if (!strcmp($key, "name"))
+				$name = $value;
+			else if (!strcmp($key, "value"))
+				$val = $value;
 		}
-	}	
+		if($name != NULL && $val != NULL && !strcmp($action, "set"))
+		{
+			setcookie($name, $val, time() + 86400, "/");
+			$name = NULL;
+			$val = NULL;
+		}
+		else if ($name != NULL && (!strcmp($action, "del") || !strcmp($action, "get")))
+		{
+			if (strcmp($action, "del") == FALSE)
+				setcookie($name, NULL, time() - 3600, "/");
+			else if (!strcmp($action, "get") && $_COOKIE[$name] != NULL)
+				echo "{$_COOKIE[$name]}\n";
+			$name = NULL;
+		}
+		//print_r($k);
+//	if ($GET["action"] === "del")
+	//	setcookie($_GET["name"], "should not exist", 1);
+	}
 ?>
