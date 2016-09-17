4c8701c7d80593aef712f45d211dde225805b0b4
Kalilo <kalilohansmann@gmail.com>
2016-09-17 15:09:52 +0200

    Fixed

--- Day04/ex00/index.php
+++ Day04/ex00/index.php
@@ -1,46 +1,31 @@
 <?php
-	session_start();
-
-	//if not null, get session username; printf()
-	$username = ($_SESSION['username'] != NULL) ? $_SESSION['username'] : "Enter username";
-	$password = ($_SESSION['password'] != NULL) ? $_SESSION['password'] : "Enter password";
+	 $username = ($_GET['login'] != NULL) ? $_GET['login'] : "enterusername";
+	 $u = ($_GET['login'] != NULL) ? $username : NULL;
+	 $password = ($_GET['passwd'] != NULL) ? $_GET['passwd'] : "password";
+	 $p = ($_GET['passwd'] != NULL) ? : NULL;
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>form.php</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
-	</head>
-	<body>
-		<div id="main">
-			<h1>Login Session</h1>
-		<div id="login">
-		<h2>Login</h2>
-		<form name="index.php" method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-		<tr>
-			<td>
-				<label>Username : </label>
-			</td>
-			<td>
-				<input id="name" name="login" placeholder="<?=$username?>" type="text">
-			</td>
-		</tr>
-			<br/>
-		<tr>
-			<td>
-				<label>Password :</label>
-			</td>
-			<td>
-				<input id="passwd" name="passwd" placeholder="<?=$password?>" type="password">
-			</td>
-		</tr>
-			<td>
-				<input name="submit" type="submit" value=" OK ">
-			</td>
-		</tr>
-			<span><?php echo $error; ?></span>
-		</form>
-		</div>
-		</div>
-	</body>
-</html>
+<body style="background-color:grey">
+<h1>Login Form</h1>
+<form method = "get" action = "<?php echo $_SERVER['PHP_SELF']; ?>" name= "index.php">
+	<tr>
+		<th>
+			<p1>Username: </p1>
+		</th>
+		<th>
+			<input type="text" name="login" placeholder="<?=$username?>" value="<?=$u?>">
+		</th>
+	</tr>
+	<br/>
+	<tr>
+		<th>
+			<p1>Password:  </p1>
+		</th>
+		<th>
+			<input type="password" name="passwd" placeholder="<?=$password?>" value="<?$p?>">
+		</th>
+	</tr>
+	<br/>
+	<input type="submit" name="submit" value=" OK ">
+	<br/>
+</form>
+</body>
