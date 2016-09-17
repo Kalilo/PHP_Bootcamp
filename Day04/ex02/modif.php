769269bf426ce0416b64fc153b9adc745423d233
OddballGreg <ghavenga@e5r8p3.jnb.42.fr>
2016-09-17 18:19:26 +0200

    modif.php for ex2 made. May not be fully functional.

---
+++ Day04/ex02/modif.php
@@ -0,0 +1,27 @@
+<?php
+	/*Check form return*/
+	if ($_POST['login'] == NULL || $_POST['oldpw'] == NULL || $_POST['newpw'] == NULL)
+		die("ERROR not all input given\n");
+	/*Set variables*/
+	$username = $_POST['login'];
+	$oldpassword = hash(whirlpool, "a6f27f471" . $_POST['oldpw']);
+	$newpassword = hash(whirlpool, "a6f27f471" . $_POST['newpw']);
+	if ($newpassword == $oldpassword)
+		die("ERROR new password same as old password\n");
+	$file = unserialize(file_get_contents("../private/passwd"));
+	if ($file === NULL)
+		die("ERROR file contents was null\n");
+	/*Scan for matching login*/
+	$ok;
+	foreach ($file as $varname =>$varval)
+		if ($varval['login'] == $username && $varval['passwd'] == $oldpassword)
+		{
+			/*Store New Password*/
+			$varval['passwd'] == $newpassword; //Might not be reinserting into $file
+			$file = serialize($file);
+			if (file_put_contents("../private/passwd", $file) === FALSE)
+				die("ERROR could not write file\n");
+			die ("OK\n");
+		}
+	die("ERROR no password set\n");
+?>
