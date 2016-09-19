b93c79f2a65cbc712dc51d1e15197258e8508a19
Kalilo <kalilohansmann@gmail.com>
2016-09-19 17:53:51 +0200

    adding updates

--- Day04/ex02/modif.php
+++ Day04/ex02/modif.php
@@ -1,16 +1,16 @@
 <?php
 	/*Check form return*/
 	if ($_POST['login'] == NULL || $_POST['oldpw'] == NULL || $_POST['newpw'] == NULL)
-		die("ERROR not all input given\n");
+		die("ERROR\n");
 	/*Set variables*/
 	$username = $_POST['login'];
 	$oldpassword = hash(whirlpool, "a6f27f471" . $_POST['oldpw']);
 	$newpassword = hash(whirlpool, "a6f27f471" . $_POST['newpw']);
 	if ($newpassword == $oldpassword)
-		die("ERROR new password same as old password\n");
+		die("ERROR\n");
 	$file = unserialize(file_get_contents("../private/passwd"));
 	if ($file === NULL)
-		die("ERROR file contents was null\n");
+		die("ERROR\n");
 	/*Scan for matching login*/
 	$user = -1;
 	while ($file[++$user])
@@ -20,7 +20,7 @@
 			$file[$user]['passwd'] = $newpassword;
 			$f = serialize($file);
 			if (file_put_contents("../private/passwd", $f) === FALSE)
-				die("ERROR could not write file\n");
+				die("ERROR\n");
 			die ("OK\n");
 		}
 	die("ERROR no password set\n");
