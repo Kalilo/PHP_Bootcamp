300d3592832d711ebae647a30bf861de6f0fec20
Kalilo <kalilohansmann@gmail.com>
2016-09-19 10:32:56 +0200

    First version

---
+++ Day05/ex01/ex01.sql
@@ -0,0 +1,2 @@
+SET autocommit=0 ; source base_student.sql ; COMMIT ;
+CREATE TABLE ft_table(id INT PRIMARY KEY AUTO_INCREMENT, login CHAR(8) NOT NULL DEFULT 'toto', kind ENUM('staff', 'student', 'other') NOT NULL, creation_date DATE NOT NULL);
