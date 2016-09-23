fd0d05eece96946fb4c9ff6184374dc6d88582af
OddballGreg <ghavenga@e5r8p3.jnb.42.fr>
2016-09-23 09:50:15 +0200

    Minor edits and map creation

--- Day08/index.php
+++ Day08/index.php
@@ -1,4 +1,11 @@
 <?php
+
+session_start();
+$time = '[' . date("h-m-s") . ']:';
+$_SESSION['log'] = "$time Commander! We're trying out new equipment! Stand by!" . "</br>" . $_SESSION['log'];
+$_SESSION['log'] = "$time Commander! Our commincations uplink has been struck! We're cut off from central. (No log found)" . "</br>" . $_SESSION['log'];
+$_SESSION['map'] = file_get_contents("map.txt");
+
 ?>
 
 <!DOCTYPE html>
@@ -16,16 +23,20 @@
 		width:  100%;
 		border: 1px solid white;
 		color: white;
+		overflow-y: scroll;
+		text-align: center;
 	}
 	.log
 	{
 		align: left;
 		color: white;
 		border: 1px solid white;
-		width: 47%;
+		width: 47.5%;
 		height: 100%;
 		display: inline-block;
 		margin: 1%;
+		vertical-align: top;
+		overflow-y: scroll;
 	}
 	.interface
 	{
@@ -33,36 +44,44 @@
 		color: white;
 		border: 1px solid white;
 		height: 100%;
-		width: 47%;
+		width: 47.5%;
 		display: inline-block;
 		margin: 1%;
+		vertical-align: top
 	}
 	.container
 	{
 		color: white;
 		height: 400px;
 	}
+	.input
+	{
+ 	display: inline-block;
+	width: 40%;
+	}
 </style>
 <body>
 
 <div class="map">
-
+<?=$_SESSION['map']?>
 </div>
 <div class="container">
 	<div class="log">
 		<div align="middle">
 			Log
 			<hr />
-		</div>
+			</div>
+		<div align="left"><?=$_SESSION['log']?></div>
 	</div>
-	<div class="interface">
-		<div align="middle">
-			Interface
-			<hr />
-			<form method = "get" action = "<?php echo $_SERVER['PHP_SELF']; ?>" name = "index.php">
+	<div class="interface"  align="middle">
+		Interface
+		<hr />
+		<div class="input" >
+			<td>
+			<form align="left" method = "get" action = "<?php echo $_SERVER['PHP_SELF']; ?>" name = "index.php">
 			<tr>
 			   <th>
-				   <pre>Username: </pre>
+				   <pre>Ship Name: </pre>
 			   </th>
 			   <th>
 				   <input type = "text" name = "login" value = "<?php echo $_SESSION['user'] ?>">
@@ -71,17 +90,21 @@
 			<br/>
 			<tr>
 			   <th>
-				   <pre>Password: </pre>
+				   <pre>Hullpoints: </pre>
 			   </th>
 			   <th>
-				   <input type = "password" name = "passwd" value = "<?php echo $_SESSION['pass'] ?>">
+				   <input type = "text" name = "passwd" value = "<?php echo $_SESSION['pass'] ?>">
 			   </th>
 			</tr>
 			<br/>
 			<br/>
-			<input type = "submit" name = "submit" value = " OK ">
+			<input type = "submit" name = "submit" value = " End Turn ">
 		  	<br/>
+			</td>
+			<td>
+			</td>
 			</form>
+			</div>
 		</div>	
 	</div>
 </div>
